434ca349868bfab2a5e7cc46718c551096c52a54
Aleko Georgiev <61247544+liquid681@users.noreply.github.com>
2021-12-15 22:29:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "dynamic_arr.h"
 
-int main() {
-    stack_t* stack = init_stack(2);
-    add(stack, 5);
-    add(stack, 10);
-    add(stack, 15);
-    pop(stack);
-    print_stack(stack);
-    back(*stack);
+int main(int argc, char **argv) {
+    stack_t* stack = init_stack(15);
+    for(int i = 1; i < argc; ++i) {
+        if(!strcmp("add", argv[i])) {
+            int val;
+            if(sscanf(argv[i+1], "%d", &val))
+                add(stack, val);
+            else 
+                printf("No value to add found.\n");
+        } else if(!strcmp("pop", argv[i])) {
+            pop(stack);
+            printf("Element popped!\n");
+        } else if(!strcmp("back", argv[i])) {
+            back(*stack);
+        } else if(!strcmp("print", argv[i])) {
+            print_stack(stack);
+        }
+    }
+
+    free(stack->vals);
+    free(stack);
 
     return 0;
 }
